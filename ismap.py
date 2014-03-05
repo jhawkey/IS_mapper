2c07bb9a1f5b86ad86470e3394147a4989a94bef
jhawkey <jhawkey@student.unimelb.edu.au>
2014-03-05 11:39:46 +1100

    testing new readset

--- ismap.py
+++ ismap.py
@@ -286,7 +286,7 @@ def main():
         three_contigHits = output_path + sample + "_3_contigHits.txt"
 
         #map to IS reference
-        #run_command(['bwa', 'mem', args.reference, forward_read, reverse_read, '>', output_sam])
+        run_command(['bwa', 'mem', args.reference, forward_read, reverse_read, '>', output_sam])
         #print(' '.join(['bwa', 'mem', args.reference, forward_read, reverse_read, '>', output_sam]))
 
         #pull unmapped reads flanking IS
