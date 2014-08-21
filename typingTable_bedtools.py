1453bc0442a14d720223b9dd23d189bd2a98f0e0
jhawkey <jhawkey@student.unimelb.edu.au>
2014-08-21 14:33:25 +1000

    fixed indent

--- typingTable_bedtools.py
+++ typingTable_bedtools.py
@@ -309,9 +309,9 @@ def main():
                    print 'not sure'
                    gene_left, gene_left_dist, gene_right, gene_right_dist = get_flanking_genes(args.reference_genbank, start, end, args.cds, args.trna, args.rrna)
                    if len(seq_results) !=0:
-                        results['region_' + str(region)] = [orient, str(start), str(end), info[6], 'Unknown', str(results[0]), str('%.2f' % results[1]), gene_left[:-1], gene_left[-1], gene_left_dist, gene_right[:-1], gene_right[-1], gene_right_dist]
-                    else:
-                        results['region_' + str(region)] = [orient, str(start), str(end), info[6], 'Unknown', 'no hit', 'no hit', gene_left[:-1], gene_left[-1], gene_left_dist, gene_right[:-1], gene_right[-1], gene_right_dist]
+                       results['region_' + str(region)] = [orient, str(start), str(end), info[6], 'Unknown', str(results[0]), str('%.2f' % results[1]), gene_left[:-1], gene_left[-1], gene_left_dist, gene_right[:-1], gene_right[-1], gene_right_dist]
+                   else:
+                       results['region_' + str(region)] = [orient, str(start), str(end), info[6], 'Unknown', 'no hit', 'no hit', gene_left[:-1], gene_left[-1], gene_left_dist, gene_right[:-1], gene_right[-1], gene_right_dist]
                 region += 1
             else:
                 #this is something else altogether - either the gap is really large or something, place it in removed_results
