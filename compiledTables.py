9a9dfeafa316fbc355e9484f3d7eb78dd781025f
jhawkey <jhawkey@student.unimelb.edu.au>
2014-08-21 13:24:20 +1000

    updating compiled tables to correctly find overlaps

--- compiledTables.py
+++ compiledTables.py
@@ -22,92 +22,105 @@ def parse_args():
 
     return parser.parse_args()
 
-def check_ranges(ranges, range_to_check, gap, orientation, unpaired = None):
+def check_ranges(ranges, range_to_check, gap, orientation):
 
-    if unpaired == None:
-        start = range_to_check[0]
-        stop = range_to_check[1]
-        #print 'this is the start and stop'
-        #print start, stop
-        #print 'these are the ranges that we are checking against'
-        ranges_list = ranges.keys()
-        #print ranges
-        #print 'this is the orientation: ' + orientation
-        for i in range(0, len(ranges_list)):
-         #   print 'these are the x and y values'
-            x = ranges_list[i][0]
-            y = ranges_list[i][1]
-            if x in range(2802000, 2804000) and y in range(2802000, 2804000):
-                print 'these are the start and stop values'
-                print start, stop
-                print 'these are the x and y values'
-                print x, y
-                print 'this is the current orientation'
-                print orientation
-                print 'this the orientation of the range that we are currently checking against'
-                checking_orientation = ranges[(ranges_list[i][0], ranges_list[i][1])]
-                print checking_orientation
-            checking_orientation = ranges[(ranges_list[i][0], ranges_list[i][1])]
-          #  print checking_orientation
-            if orientation == checking_orientation:
-           #     print 'so the orientation matches'
-            #    print 'these are the start and stop values'
-             #   print start, stop
-                #print 'these are the x and y values'
-                #print x, y
-                if orientation == "5' to 3'":
-                    if start >= (x - gap) and start <= (y + 1):
-                        print 'we are the start part, these are the two conditions being checked'
-                        print start >= (x - gap)
-                        print start <= (y + 1)
-                        new_start = min(x, start)
-                        new_end = max(y, stop)
-                        print 'this is the start and stop that were successful'
-                        print start, stop
-                        return ranges_list[i], (new_start, new_end), orientation
-                    elif stop >= x and stop <= (y + gap + 1):
-                        print 'we are in the stop part, two conditions'
-                        print stop >= x
-                        print stop <= (y + gap + 1)
-                        new_start = min(x, start)
-                        new_end = max(y, stop)
-                        print 'this is the start and stop that were successful'
-                        print start, stop
-                        return ranges_list[i], (new_start, new_end), orientation
-                else:
-                    print 'this is for 3 prime to 5 prime orientation'
-                    if start <= (x + gap) and start > y:
-                        print 'we are at the start part, two conditions'
-                        print start <= (x + gap)
-                        print start > y
-                        new_start = max(x, start)
-                        new_end = min(y, stop)
-                        print 'this is the start and stop that were successful'
-                        print start, stop
-                        return ranges_list[i], (new_start, new_end), orientation
-                    elif stop >= (y - gap) and stop < x:
-                        print 'we are at the stop part, two conditions'
-                        print stop >= (y - gap) 
-                        print stop < x
-                        new_start = max(x, start)
-                        new_end = min(y, stop)
-                        print 'this is the start and stop that were successful'
-                        print start, stop
-                        return ranges_list[i], (new_start, new_end), orientation
+    start = range_to_check[0]
+    stop = range_to_check[1]
+
+    range_list = ranges.keys()
+
+    for i in range(0, len(range_list)):
+        if orientation == ranges[(range_list[i][0], range_list[i][1])]:
+            x = range_list[i][0]
+            y = range_list[i][1]
+            if orientation == 'F':
+                if x in range(start - gap, start) or x in range(stop, stop + gap):
+                    #these ranges overlap
+                    new_start = min(x, start)
+                    new_end = max(y, stop)
+                    return range_list[i], (new_start, new_end), orientation
+                elif y in range(start - gap, start) or y in range(stop, stop + gap):
+                    #these ranges also overlap
+                    new_start = min(x, start)
+                    new_end = min(y, stop)
+                    return range_list[i], (new_start, new_end), orientation
+            elif orientation == 'R':
+                if x in range(start - gap, start) or x in range(stop, stop + gap):
+                    #these ranges overlap
+                    new_start = min(x, start)
+                    new_end = max(y, stop)
+                    return range_list[i], (new_start, new_end), orientation
+                elif y in range(start - gap, start) or y in range(stop, stop + gap):
+                    #these ranges also overlap
+                    new_start = min(x, start)
+                    new_end = min(y, stop)
+                    return range_list[i], (new_start, new_end), orientation
+    return False, False, False
+
+'''
+    print 'we are at the top of the check_ranges function'
+    print 'this is the start and stop'
+    print start, stop
+    print 'these are the ranges that we are checking against'
+    ranges_list = ranges.keys()
+    print ranges
+    print 'this is the orientation: ' + orientation
+    for i in range(0, len(ranges_list)):
+        print 'these are the x and y values'
+        x = ranges_list[i][0]
+        y = ranges_list[i][1]
+        print x, y
+        checking_orientation = ranges[(ranges_list[i][0], ranges_list[i][1])]
+        print checking_orientation
+        if orientation == checking_orientation:
+            print 'so the orientation matches'
+            print 'these are the start and stop values'
+            print start, stop
+            print 'these are the x and y values'
+            print x, y
+            if orientation == 'F':
+                if start >= (x - gap) and start <= (y + 1):
+                    print 'we are the start part, these are the two conditions being checked'
+                    print start >= (x - gap)
+                    print start <= (y + 1)
+                    new_start = min(x, start)
+                    new_end = max(y, stop)
+                    print 'this is the start and stop that were successful'
+                    print start, stop
+                    return ranges_list[i], (new_start, new_end), orientation
+                elif stop >= x and stop <= (y + gap + 1):
+                    print 'we are in the stop part, two conditions'
+                    print stop >= x
+                    print stop <= (y + gap + 1)
+                    new_start = min(x, start)
+                    new_end = max(y, stop)
+                    print 'this is the start and stop that were successful'
+                    print start, stop
+                    return ranges_list[i], (new_start, new_end), orientation
+            else:
+                print 'this is for R orientation'
+                if start <= (x + gap) and start > y:
+                    print 'we are at the start part, two conditions'
+                    print start <= (x + gap)
+                    print start > y
+                    new_start = max(x, start)
+                    new_end = min(y, stop)
+                    print 'this is the start and stop that were successful'
+                    print start, stop
+                    return ranges_list[i], (new_start, new_end), orientation
+                elif stop >= (y - gap) and stop < x:
+                    print 'we are at the stop part, two conditions'
+                    print stop >= (y - gap) 
+                    print stop < x
+                    new_start = max(x, start)
+                    new_end = min(y, stop)
+                    print 'this is the start and stop that were successful'
+                    print start, stop
+                    return ranges_list[i], (new_start, new_end), orientation
             #print 'the orientation did not match'
 
         return False, False, False
-    elif unpaired == True:
-        coord = range_to_check
-        for i in range(0, len(ranges)):
-            x = min(ranges[i][0], ranges[i][1])
-            y = max(ranges[i][0], ranges[i][1])
-            if coord in range(x-gap, y+1):
-                return ranges[i], True, False
-            elif coord in range(x, y+gap+1):
-                return ranges[i], True, False
-           
-        return False, False, False
+        '''
 
 def get_ref_positions(reference, is_query, positions_dict, orientation_dict):
 
@@ -125,9 +138,9 @@ def get_ref_positions(reference, is_query, positions_dict, orientation_dict):
             if float(info[3]) >= 95 and float(info[4])/float(info[1]) * 100 >= 95:
                 positions_dict[(int(info[6]), int(info[7]))][ref_name] = '+'
                 if int(info[6]) > int(info[7]):
-                    orientation_dict[(int(info[6]), int(info[7]))] = "3' to 5'"
+                    orientation_dict[(int(info[6]), int(info[7]))] = 'R'
                 else:
-                    orientation_dict[(int(info[6]), int(info[7]))] = "5' to 3'"
+                    orientation_dict[(int(info[6]), int(info[7]))] = 'F'
 
     return positions_dict, orientation_dict, ref_name
 
@@ -208,7 +221,6 @@ def get_flanking_genes(reference, positions):
 
     return pos_gene_start, pos_gene_end
 
-
 def main():
 
     args = parse_args()
@@ -217,10 +229,10 @@ def main():
     list_of_isolates = []
 
     list_of_positions = collections.defaultdict(dict) # key1 = pos, key2 = isolate, value = +/-
-    unpaired_hits = {}
     position_orientation = {}
 
     list_of_positions, position_orientation, ref_name = get_ref_positions(args.reference_fasta, args.seq, list_of_positions, position_orientation)
+    print position_orientation
 
     for result_file in unique_results_files:
         isolate = result_file.split('__')[0]
@@ -229,7 +241,7 @@ def main():
         with open(result_file) as file_open:
             for line in file_open:
                 if header == 0:
-                    header = header + 1
+                    header += 1
                 elif 'No hits found' not in line and line != '':
                     #print isolate
                     info = line.strip('\n').split('\t')
@@ -237,21 +249,11 @@ def main():
                     orientation = info[1]
                     is_start = int(info[2])
                     #print is_start
-                    if info[4] != '':
-                        is_end = int(info[5])
-                        #print is_end
-                    else:
-                        #print 'this is not a paired hit'
-                        is_end = info[5]
-                        if isolate not in unpaired_hits:
-                            #print 'adding it to the unpaired hits'
-                            unpaired_hits[isolate] = [is_start]
-                        else:
-                            #print 'adding it to the unpaired hits (else)'
-                            unpaired_hits[isolate].append(is_start)
-                    if (is_start, is_end) not in list_of_positions and is_end != '':
+                    is_end = int(info[3])
+                    #print is_end
+                    if (is_start, is_end) not in list_of_positions:
                         if list_of_positions.keys() != []:
-                            old_range, new_range, new_orientation = check_ranges(position_orientation, (is_start, is_end), args.gap, orientation, unpaired=None)
+                            old_range, new_range, new_orientation = check_ranges(position_orientation, (is_start, is_end), args.gap, orientation)
                             #print old_range, new_range, new_orientation
                             if old_range != False:
                                 store_values = list_of_positions[old_range]
@@ -266,24 +268,9 @@ def main():
                         else:
                             list_of_positions[(is_start, is_end)][isolate] = '+'
                             position_orientation[(is_start, is_end)] = orientation
-                    elif (is_start, is_end) in list_of_positions and is_end != '':
+                    elif (is_start, is_end) in list_of_positions:
                         list_of_positions[(is_start, is_end)][isolate] = '+'
                     #print position_orientation
-        
-    # dealing with unpaired hits after files have been read in
-    paired_hits = list_of_positions.keys()
-    for isolate in unpaired_hits:
-        for hit in unpaired_hits[isolate]:
-            range_hit, boolean, orientation = check_ranges(paired_hits, hit, args.gap, orientation=None, unpaired=True)
-            #print range_hit, boolean
-            if boolean == True:
-                list_of_positions[range_hit][isolate] = '+*'
-            else:
-                list_of_positions[(hit, hit+1)][isolate] = '+?'
-
-    #print list_of_positions
-    #print unpaired_hits
-    #print list_of_isolates
 
     # ordering positions from smallest to largest for final table output
     order_position_list = list(OrderedDict.fromkeys(list_of_positions.keys()))
