e7b373ad00a57c7b978344c4bf9ceda9f9dc4b8f
jhawkey <jhawkey@student.unimelb.edu.au>
2015-02-20 10:45:47 +1100

    attempting to identify high memory usage

--- scripts/ismap.py
+++ scripts/ismap.py
@@ -28,6 +28,7 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import generic_dna
+import resource
 try:
     from version import ismap_version
 except:
@@ -356,16 +357,20 @@ def multi_to_single(genbank, name, output):
 
 def extract_clipped_reads(fastq_file, size):
 
+    print ('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
     clipped = SeqIO.parse(fastq_file, "fastq")
+    print ('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
     left_reads = []
     right_reads = []
     for fastq in clipped:
+        print ('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
         if len(fastq.seq) <= size:
             # These are forward reads, so are on the right-hand side
             if fastq.name[-2:] == '_1':
                 right_reads.append(fastq)
             else:
                 left_reads.append(fastq)
+    print ('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
     return left_reads, right_reads
 
 def main():
@@ -447,7 +452,9 @@ def main():
             # Map to IS query
             run_command(['bwa', 'mem', query, forward_read, reverse_read, '>', output_sam], shell=True)
             # Run Samblaster to extract softclipped reads
+            print ('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
             run_command(['samblaster', '-u', clipped_reads, '-i', output_sam, '-o /dev/null', '-e', '--minClipSize', args.min_clip], shell=True)
+            print ('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
             # Pull unmapped reads flanking IS
             run_command(['samtools view', '-Sb', '-f 36', output_sam, '>', five_bam], shell=True)
             run_command(['samtools view', '-Sb', '-f 4', '-F 40', output_sam, '>', three_bam], shell=True)
@@ -455,13 +462,17 @@ def main():
             run_command(['bedtools', 'bamtofastq', '-i', five_bam, '-fq', five_reads], shell=True)
             run_command(['bedtools', 'bamtofastq', '-i', three_bam, '-fq', three_reads], shell=True)
             # Add corresponding clipped reads to their respective left and right ends
+            print ('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
             logging.info('Filtering soft clipped reads, selecting reads that are <= ' + str(args.max_clip) + 'bp')
             left_clipped, right_clipped = extract_clipped_reads(clipped_reads, args.max_clip)
+            print ('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
             logging.info('Writing out left and right soft clipped reads')
             SeqIO.write(left_clipped, left_clipped_reads, 'fastq')
             SeqIO.write(right_clipped, right_clipped_reads, 'fastq')
+            print ('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
             run_command(['cat', left_clipped_reads, five_reads, '>', final_left_reads], shell=True)
             run_command(['cat', right_clipped_reads, three_reads, '>', final_right_reads], shell=True)
+            print ('Memory usage: %s (kb)' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
 
             # Create BLAST database for IS query
             check_blast_database(query)
