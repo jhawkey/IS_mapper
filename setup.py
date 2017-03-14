f2df34a8ebcbe4a097c771c6607b69a9de8d351c
Jane Hawkey <janehawkey@unimelb.edu.au>
2017-03-15 09:20:20 +1100

    added sg slurm script to install script

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='hawkey.jane@gmail.com',
     packages=['ismap'],
     scripts=['scripts/binary_table.py', 'scripts/compiled_table.py', 'scripts/create_genbank_table.py',
-            'scripts/slurm_ismap.py', 'scripts/create_typing_out.py'],
+            'scripts/slurm_ismap.py', 'scripts/create_typing_out.py', 'scripts/slurm_ismap_sg.py'],
     entry_points={
         'console_scripts': ['ismap = ismap.ismap:main']
     },
