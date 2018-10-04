8357ae1a3aba7313a6af2973ca51c431343e9f96
Jane Hawkey <jhawkey@student.unimelb.edu.au>
2018-10-04 13:40:16 +1000

    updated setup script

--- setup.py
+++ setup.py
@@ -1,36 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='ISMapper',
-    version='0.1.5.1',
-    author='Jane Hawkey',
-    author_email='hawkey.jane@gmail.com',
+setuptools.setup(
+    name="ISMapper",
+    version="2.0",
+    author="Jane Hawkey",
+    description="Detection of insertion sequence locations from short read sequence data",
+    url="https://github.com/jhawkey/IS_mapper",
     packages=['ismap'],
-    scripts=['scripts/binary_table.py', 'scripts/compiled_table.py', 'scripts/create_genbank_table.py',
-            'scripts/slurm_ismap.py', 'scripts/create_typing_out.py', 'scripts/slurm_ismap_sg.py'],
+    scripts=['scripts/ismap.py', 'scripts/compiled_table.py', 'scripts/create_output.py', 'scripts/mapping_to_query.py',
+             'scripts/mapping_to_ref.py', 'scripts/read_grouping.py', 'scripts/run_commands.py'],
     entry_points={
-        'console_scripts': ['ismap = ismap.ismap:main']
+        'console_scripts': ['ismap = ismap:main']
     },
     package_dir = {'ismap': 'scripts'},
-    #package_data={'srst2': ['data/resistance.*']},
-    url='http://jhawkey.github.io/IS_mapper/',
     license='LICENSE.txt',
-    description='Discovering Insertion Sequences from Short Read Sequence Data',
-    long_description=('This program is designed to take Illumina'
-                      'sequence data, a query gene (such as an insertion sequence)'
-                      'and a reference genome, either for finding novel positions in,'
-                      'or an assembly where the query is either assembled or broken over'
-                      'contigs.'),
-    install_requires=[
-        # Although we depend on scipy, which depends on numpy, we don't
-        # specify the dependencies here because they don't play well with
-        # any Python installing system, such as pip or easy_install.
-        # So we assume the user has already installed the dependencies
-        # themselves.
-        #"numpy >= 1.7.1",
-        #"scipy >= 0.12.0",
-        #"biopython == 1.63",
-    ],
 )
