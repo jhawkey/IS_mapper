b73907a70bf7a9bb6aad8b0b5783a2ddffd69d40
Jane Hawkey <jhawkey@student.unimelb.edu.au>
2018-10-25 08:26:27 +1100

    added version.py code back

--- scripts/version.py
+++ scripts/version.py
@@ -2,3 +2,6 @@
 # This solution was suggested on Stack Overflow:
 # http://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
 
+import pkg_resources  # part of setuptools
+
+ismap_version = pkg_resources.require("ISMapper")[0].version
